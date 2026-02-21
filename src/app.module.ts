c918a7e8b268409498708fa95a34d92983cc72e7
akuma19 <kellyhandy31@gmail.com>
2026-02-21 13:07:11 +0100

    feat(): database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,17 @@ import { AppService } from "./app.service";
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
         const host = config.get<string>("DB_HOST");
-        const port = config.get<number>("DB_PORT");
+        const port = config.get<string>("DB_PORT") || "5432";
         const username = config.get<string>("DB_USER");
         const password = config.get<string>("DB_PASSWORD");
         const database = config.get<string>("DB_NAME");
-        if (!host || !port || !username || !database || !password) {
+        if (
+          !host ||
+          isNaN(Number(port)) ||
+          !username ||
+          !database ||
+          !password
+        ) {
           throw new Error("Missing required database environment variables");
         }
 
